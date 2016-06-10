b9d819c3d05eb9c8c255dd2cb915900ddca1a655
Pead Nong <anuchit.prasertsang@gmail.com>
2016-06-10 17:36:30 +0700

    create file js index.html

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+var path = require('path');
+
+module.exports = {
+  entry: [
+    './src/index.js'
+  ],
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js',
+    publicPath: '/public/'
+  },
+  resolve: {
+    extensions: ['', '.js', '.ts', '.tsx']
+  }
+};
